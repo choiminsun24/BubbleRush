727258e4f9cef80deeaa5c813d0bbf28db45185b
choiminsun <backwonand@sookmyung.ac.kr>
2023-06-27 17:17:16 +0900

    월드 선택 UI 연결 마무리

--- Assets/Scripts/SelectStage.cs
+++ Assets/Scripts/SelectStage.cs
@@ -8,6 +8,7 @@ public class SelectStage : MonoBehaviour
     public Material nonBlur;
     public Material blur;
     public SpriteRenderer background;
+    public GameObject stagePanel;
 
     public Animator anim;
 
@@ -17,25 +18,36 @@ public class SelectStage : MonoBehaviour
         {
             case 1:
                 anim.SetInteger("num", 1);
+                Invoke("OpenSelectStage", 0.8f);
                 break;
             case 2:
                 anim.SetInteger("num", 2);
+                Invoke("OpenSelectStage", 0.8f);
                 break;
             case 3:
                 anim.SetInteger("num", 3);
+                Invoke("OpenSelectStage", 0.8f);
                 break;
             case 4:
                 anim.SetInteger("num", 4);
+                Invoke("OpenSelectStage", 0.8f);
                 break;
             default:
                 anim.SetInteger("num", 0);
+                Invoke("CloseSelectStage", 0.8f);
                 break;
         }
-        Invoke("OpenSelectStage", 0.8f);
     }
 
     public void OpenSelectStage()
     {
+        stagePanel.SetActive(true);
         background.material = blur;
+    }
+
+    public void CloseSelectStage()
+    {
+        stagePanel.SetActive(false);
+        background.material = nonBlur;
     }
 }
