c8f5ab03f94b303648131681acd8e05f9e7a21cf
choiminsun <backwonand@sookmyung.ac.kr>
2023-07-16 19:06:48 +0900

    Buff 생성

---
+++ Assets/Scripts/BuffQuest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffQuest : MonoBehaviour
+{
+    public InGameData data;
+
+    public void Medicine()
+    {
+        //공격속도를 올려줘요
+    }
+
+    public void ArtificialEyes()
+    {
+        //사거리 증가
+    }
+
+    public void BloodFlower()
+    {
+        //공격력 증가
+    }
+
+    public void PainShadow()
+    {
+        //공격속도 증가
+    }
+
+    public void Weak()
+    {
+        //공격력 증가
+    }
+}
