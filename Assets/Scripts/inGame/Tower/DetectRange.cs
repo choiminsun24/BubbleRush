4f7e46210268e286d6edeb2aea4889b04f294b69
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-08-28 19:01:58 +0900

    타워 배치 범위 오류 해결

--- Assets/Scripts/inGame/Tower/DetectRange.cs
+++ Assets/Scripts/inGame/Tower/DetectRange.cs
@@ -10,18 +10,19 @@ public class DetectRange : MonoBehaviour
     [SerializeField] private string category;   // 타워 종류
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Tower")
-        {
-            hit = false;
-            // Red
-            sprite.color = new Color(1f, 0f, 0f, sprite.color.a);
-        }
 
-        else if(other.tag == category)
+        if(other.tag == category)
         {
             hit = true;
             // Gray
             sprite.color = new Color(0.5f, 0.5f, 0.5f, sprite.color.a);
+        }
+
+        if (other.tag == "Tower")
+        {
+            hit = false;
+            // Red
+            sprite.color = new Color(1f, 0f, 0f, sprite.color.a);
         }
 
         else if(other.tag == "Player")
@@ -30,20 +31,31 @@ public class DetectRange : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) 
+    private void OnTriggerStay2D(Collider2D other)
     {
         if(other.tag == "Tower")
-        {
-            hit = true;
-            // Gray
-            sprite.color = new Color(0.5f, 0.5f, 0.5f, sprite.color.a);
-        }
-
-        else if(other.tag == category)
         {
             hit = false;
             // Red
             sprite.color = new Color(1f, 0f, 0f, sprite.color.a);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+
+        if(other.tag == category)
+        {
+            hit = false;
+            // Red
+            sprite.color = new Color(1f, 0f, 0f, sprite.color.a);
+        }
+
+        if (other.tag == "Tower")
+        {
+            hit = true;
+            // Gray
+            sprite.color = new Color(0.5f, 0.5f, 0.5f, sprite.color.a);
         }
 
         else if(other.gameObject.tag == "Player")
@@ -51,6 +63,7 @@ public class DetectRange : MonoBehaviour
             tc.RemoveEnemies(other.gameObject);
         }
     }
+
     private void Awake()
     {
         sprite = GetComponent<SpriteRenderer>();
