93bf8004267d55677e7ebc72ba614a34de340d42
choiminsun <backwonand@sookmyung.ac.kr>
2023-08-28 22:47:56 +0900

    화면 비율 조정, 선택한 카드 확인 기능 추가

--- Assets/Scripts/inGame/Buff.cs
+++ Assets/Scripts/inGame/Buff.cs
@@ -6,20 +6,6 @@ using UnityEngine.UI;
 
 public class Buff : MonoBehaviour
 {
-    /*
-     * 필요한 것 
-     * UI
-     *  - 버프 종류 <> 테두리
-     *  - 버프 이미지 <> 이미지
-     *  - Name <> 버프 타이틀
-     *  - 버프 Content <> 버프 content
-     *  
-     *내부 처리
-     *  - 버프 종류 <> 버프 메서드
-     *  - 버프 수치 <> 버프 value
-     *  - 버프 대상 <>
-     */
-
     //데이터
     public InGameData data;
     private List<Dictionary<string, string>> textData;
@@ -28,11 +14,17 @@ public class Buff : MonoBehaviour
     //UI 동작
     public Transform canvas; //소속된 Canvas
     public GameObject Box; //UI창
-    public GameObject[] position; //생성 위치
+    public Transform[] position; //생성 위치
     public Sprite[] images;
+
+    public GameObject my;
 
     //선택지
     private int[] num; //선택된 번호
+    private int mineNum = 0;
+
+    //선택된
+    public Transform[] mine;
 
     //프로그램
     private static Buff instance;
@@ -59,12 +51,15 @@ public class Buff : MonoBehaviour
     public void Start()
     {
         Box.SetActive(false);
+        my.SetActive(false);
 
         //buffNum를 textData 행 번호(배열 인덱스)로 초기화
         for (int i = 0; i < textData.Count; i++)
         {
             buffNum.Add(i);
         }
+
+        //play();
     }
 
     //선택지 on
@@ -94,26 +89,32 @@ public class Buff : MonoBehaviour
         //2. 카드 UI 세팅
         for (int i = 0; i < num.Length; i++)
         {
-            Transform tf = position[i].GetComponent<Transform>();
+            Transform tf = position[i];
 
-            //카드 프레임
-            if (textData[num[i]]["Type"].Equals("NatureBless")) //버프 카드
-            {
-                tf.gameObject.GetComponent<Image>().sprite = images[0];
-            }
-            else if (textData[num[i]]["Type"].Equals("DarknessCurse")) //디버프 카드
-            {
-                tf.gameObject.GetComponent<Image>().sprite = images[1];
-            }
-            else //리워드 카드
-            {
-                tf.gameObject.GetComponent<Image>().sprite = images[2];
-            }
-
-            tf.GetChild(0).GetComponent<Text>().text = textData[num[i]]["Name"]; //Title
-            tf.GetChild(1).GetComponent<Text>().text = textData[num[i]]["Description"]; //Content
-            tf.GetChild(2).GetComponent<Image>().sprite = Resources.Load<Sprite>(textData[i]["Directory"]);
+            cardSetting(tf, textData[num[i]]);
         }
+    }
+
+    //세팅할 카드, 세팅할 정보
+    private void cardSetting(Transform tf, Dictionary<string, string> target)
+    {
+        //카드 프레임
+        if (target["Type"].Equals("NatureBless")) //버프 카드
+        {
+            tf.gameObject.GetComponent<Image>().sprite = images[0];
+        }
+        else if (target["Type"].Equals("DarknessCurse")) //디버프 카드
+        {
+            tf.gameObject.GetComponent<Image>().sprite = images[1];
+        }
+        else //리워드 카드
+        {
+            tf.gameObject.GetComponent<Image>().sprite = images[2];
+        }
+
+        tf.GetChild(0).GetComponent<Text>().text = target["Name"]; //Title
+        tf.GetChild(1).GetComponent<Text>().text = target["Description"]; //Content
+        tf.GetChild(2).GetComponent<Image>().sprite = Resources.Load<Sprite>(target["Directory"]);
     }
 
     //선택 후 처리
@@ -122,6 +123,7 @@ public class Buff : MonoBehaviour
         Box.SetActive(false); //선택 창 제거
         buffNum.RemoveAt(num[n]); //버프 넘에서 선택 번호 제외. -> 다음에 뽑히지 않도록 함.
         Dictionary<string, string> choice = textData[num[n]]; //선택된 행
+        cardSetting(mine[mineNum], choice);
 
 
         //내부 버프 효과 **************************값 변경 미적용***********************
@@ -141,5 +143,10 @@ public class Buff : MonoBehaviour
         }
 
         //data.BuffATKS(1.06f); 예시문
+    }
+
+    public void watchChoice()
+    {
+        my.SetActive(!my.activeSelf);
     }
 }
