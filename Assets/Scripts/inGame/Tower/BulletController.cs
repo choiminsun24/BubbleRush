ef170d3e90ce345ebba1732d966173ca199d71dc
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-09-02 00:54:53 +0900

    버블 죽을 때 bullet 오류 해결

--- Assets/Scripts/inGame/Tower/BulletController.cs
+++ Assets/Scripts/inGame/Tower/BulletController.cs
@@ -38,8 +38,7 @@ public class BulletController : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            other.gameObject.GetComponent<Enemy>().takeDamage(ATK, (int)expression);
-            Destroy(gameObject);
+            other.gameObject.GetComponent<Enemy>().takeDamage(ATK, (int)expression, gameObject);
         }
     }
 }
